4298262d4a2fd29e48d87d7d81d06b0d25156a0a
Maxim <maximyurinmusic@gmail.com>
2023-02-23 21:19:28 +0200

    Added if instruction

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 // JavaScripts homeworks 1
 
-let firstNumber = prompt('Введите первое число');
-let secondNumber = prompt('Введите второе число');
+let firstNumber = +prompt('Введите первое число');
+let secondNumber = +prompt('Введите второе число');
 
-alert(`Сумма ваших чисел: ${Number(firstNumber) + Number(secondNumber)}`);
-alert(`Разница ваших чисел: ${Number(firstNumber) - Number(secondNumber)}`);
-alert(`Результат умножения ваших чисел: ${Number(firstNumber) * Number(secondNumber)}`);
-alert(`Результат деления ваших чисел: ${Number(firstNumber) / Number(secondNumber)}`);
-alert(`Максимальное число из введённых вами чисел: ${Math.max(Number(firstNumber), Number(secondNumber))}`);
-alert(`Первое число: ${firstNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
-alert(`Второе число: ${secondNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
-
-
+if (firstNumber && secondNumber) {
+    alert(`Сумма ваших чисел: ${firstNumber + secondNumber}`);
+    alert(`Разница ваших чисел: ${firstNumber - secondNumber}`);
+    alert(`Результат умножения ваших чисел: ${firstNumber * secondNumber}`);
+    alert(`Результат деления ваших чисел: ${firstNumber / secondNumber}`);
+    alert(`Максимальное число из введённых вами чисел: ${Math.max(firstNumber, secondNumber)}`);
+    alert(`Первое число: ${firstNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
+    alert(`Второе число: ${secondNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
+} else {
+    alert('Одно из значений не было введено');
+}
 
 
