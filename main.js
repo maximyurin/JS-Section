4dafc6be22bd409d917c7a40d15ddecc589d5a9b
Maxim <maximyurinmusic@gmail.com>
2023-02-23 20:56:45 +0200

    All fixed

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-let firstNumber = prompt('Введите первое число')
-let secondNumber = prompt('Введите второе число')
+// JavaScripts homeworks 1
 
-const calcNumbers = (a, b) => `
-Ваши числа: ${a} и ${b}.
-Сумма ваших чисел: ${+a + +b}.
-Разница ваших чисел: ${a - b}.
-Результат умножения ваших чисел: ${a * b}.
-Результат деления ваших чисел: ${a / b}.
-Максимальное число из введённых вами чисел: ${Math.max(a, b)}.
-Первое число: ${a % 2 === 0 ? 'чётное' : 'не чётное'}.
-Второе число: ${b % 2 === 0 ? 'чётное' : 'не чётное'}.`;
+let firstNumber = prompt('Введите первое число');
+let secondNumber = prompt('Введите второе число');
 
-alert(calcNumbers(firstNumber, secondNumber))
+alert(`Сумма ваших чисел: ${Number(firstNumber) + Number(secondNumber)}`);
+alert(`Разница ваших чисел: ${Number(firstNumber) - Number(secondNumber)}`);
+alert(`Результат умножения ваших чисел: ${Number(firstNumber) * Number(secondNumber)}`);
+alert(`Результат деления ваших чисел: ${Number(firstNumber) / Number(secondNumber)}`);
+alert(`Максимальное число из введённых вами чисел: ${Math.max(Number(firstNumber), Number(secondNumber))}`);
+alert(`Первое число: ${firstNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
+alert(`Второе число: ${secondNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
+
+
+
+
