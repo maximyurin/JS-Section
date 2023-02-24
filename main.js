1f6ba1e1af173a387fe51fc7af7ea136edaaf7b3
Maxim <maximyurinmusic@gmail.com>
2023-02-24 23:14:45 +0200

    JS Homework 2 is finished

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-// JavaScripts homeworks 1
+// JavaScripts homeworks 2
 
-let firstNumber = +prompt('Введите первое число');
-let secondNumber = +prompt('Введите второе число');
+let firstNumber = prompt('Введите первое число');
+let secondNumber = prompt('Введите второе число');
 
 if (firstNumber && secondNumber) {
-    alert(`Сумма ваших чисел: ${firstNumber + secondNumber}`);
-    alert(`Разница ваших чисел: ${firstNumber - secondNumber}`);
-    alert(`Результат умножения ваших чисел: ${firstNumber * secondNumber}`);
-    alert(`Результат деления ваших чисел: ${firstNumber / secondNumber}`);
-    alert(`Максимальное число из введённых вами чисел: ${Math.max(firstNumber, secondNumber)}`);
-    alert(`Первое число: ${firstNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
-    alert(`Второе число: ${secondNumber % 2 === 0 ? 'чётное' : 'не чётное'}`);
+    let num1 = Number(firstNumber);
+    let num2 = Number(secondNumber);
+    alert(`Сумма ваших чисел: ${num1 + num2}`);
+    if (num1 < num2) {
+        if (confirm('Второе число больше первого, вы получите отрицательное значение. Вы уверены что хотите продолжить операцию?')) {
+            alert(`Разница ваших чисел: ${num1 - num2}`);
+        }
+    } else if (num1 > num2) {
+        alert(`Разница ваших чисел: ${num1 - num2}`);
+    }
+    alert(`Результат умножения ваших чисел: ${num1 * num2}`);
+    if (num2 === 0) {
+        alert('На ноль делить нельзя, введите заново второе число');
+        do {
+            num2 = prompt('Введите ещё раз второе число');
+        } while (+num2 === 0);
+    }
+    alert(`Результат деления ваших чисел: ${num1 / num2}`);
+    alert(`Максимальное число из введённых вами чисел: ${Math.max(num1, num2)}`);
+    alert(`Первое число: ${num1 % 2 === 0 ? 'чётное' : 'не чётное'}`);
+    alert(`Второе число: ${num2 % 2 === 0 ? 'чётное' : 'не чётное'}`);
+    alert('Спасибо за внимание! Слава Украине!');
 } else {
-    alert('Одно из значений не было введено');
+    alert('Одно из чисел не было введено');
 }
-
 
