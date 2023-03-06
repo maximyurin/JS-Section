7c6138c6ba3b12e299a3143a0d1005a943105865
Maxim <maximyurinmusic@gmail.com>
2023-03-06 22:08:52 +0200

    Homework 5 is finished

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-/* JavaScripts homeworks 1 */
+/* JavaScripts homeworks 5 Objects */
 
-let firstNumber = prompt("Введите первое число");
-let secondNumber = prompt("Введите второе число");
+// Напишіть функцію, яка буде перевіряти обʼєкт на пустоту
 
-if (firstNumber && secondNumber) {
-  let num1 = Number(firstNumber);
-  let num2 = Number(secondNumber);
-  alert(`Сумма ваших чисел: ${num1 + num2}`);
-  alert(`Разница ваших чисел: ${num1 - num2}`);
-  alert(`Результат умножения ваших чисел: ${num1 * num2}`);
-  alert(`Результат деления ваших чисел: ${num1 / num2}`);
-  alert(`Максимальное число из введённых вами чисел: ${Math.max(num1, num2)}`);
-  alert(`Первое число: ${num1 % 2 === 0 ? "чётное" : "не чётное"}`);
-  alert(`Второе число: ${num2 % 2 === 0 ? "чётное" : "не чётное"}`);
-} else {
-  alert("Одно из значений не было введено");
-}
+const isEmptyObject = (object) =>
+  Object.keys(object).length === 0 ? `Объект пустой` : "Объект не пустой";
+const myFirstObject = { name: "Maxim" };
+const mySecondObject = {};
+console.log(isEmptyObject(myFirstObject));
+console.log(isEmptyObject(mySecondObject));
+
+// 2. Опишіть обʼєкт user з полями name, age та методом sayHello,
+// результатом виклику якого має бути повідомлення “Привіт, я name, мені age років”
+
+const user = {
+  name: "Maxim",
+  age: 40,
+  sayHello() {
+    console.log(`Привіт, я ${this.name}, мені ${this.age} років!`);
+  },
+};
+user.sayHello();
+
+// 3.Напишіть обʼєкт калькулятор. Він має містити метод ask, sum та mul.
+// При виклику методу ask обʼєкт має запросити 2 числа та зберегти їх, як свої властивості.
+// При виклику sum - вивести суму збережених чисел, при виклику mul - додаток чисел.
+
+const calculator = {
+  firstNumber: this.firstNumber,
+  secondNumber: this.secondNumber,
+  ask() {
+    this.firstNumber = Number(prompt("Введите первое число!"));
+    this.secondNumber = Number(prompt("Введите второе число!"));
+  },
+  sum() {
+    return this.firstNumber + this.secondNumber;
+  },
+  mul() {
+    return this.firstNumber * this.secondNumber;
+  },
+};
