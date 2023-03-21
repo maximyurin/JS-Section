37c9bf7ff7c6af81f4390984dfea20c3554b3401
Maxim <maximyurinmusic@gmail.com>
2023-03-21 21:07:14 +0200

    Homework 6 is finished

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
-/* JavaScripts homeworks 1 */
+/* JavaScripts homeworks 6 */
 
-let firstNumber = prompt("Введите первое число");
-let secondNumber = prompt("Введите второе число");
+// Функция-конструктор Accumulator, включающая 2 собстенных метода increment и decrement.
 
-if (firstNumber && secondNumber) {
-  let num1 = Number(firstNumber);
-  let num2 = Number(secondNumber);
-  alert(`Сумма ваших чисел: ${num1 + num2}`);
-  alert(`Разница ваших чисел: ${num1 - num2}`);
-  alert(`Результат умножения ваших чисел: ${num1 * num2}`);
-  alert(`Результат деления ваших чисел: ${num1 / num2}`);
-  alert(`Максимальное число из введённых вами чисел: ${Math.max(num1, num2)}`);
-  alert(`Первое число: ${num1 % 2 === 0 ? "чётное" : "не чётное"}`);
-  alert(`Второе число: ${num2 % 2 === 0 ? "чётное" : "не чётное"}`);
-} else {
-  alert("Одно из значений не было введено");
+function Accumulator(initialValue) {
+  this.value = initialValue;
+  // Метод Increment
+  this.increment = function () {
+    return this.value++;
+  };
+  // Метод Decrement
+  this.decrement = function () {
+    return this.value--;
+  };
 }
+
+// Функция-конструктор CancelableAccumulator, наследующая все методы Accumulator и имеющая свой собственный метод Clear.
+
+function CancelableAccumulator(initialValue) {
+  Accumulator.call(this, initialValue);
+  this.initialValue = initialValue;
+  // Метод Clear
+  this.clear = function () {
+    return (this.value = this.initialValue);
+  };
+}
+
+// Тесты
+
+console.log("Екземпляр прототипа Accumulator");
+const myAccumulator = new Accumulator(54);
+console.log(`Переданное значение ${myAccumulator.value}`);
+myAccumulator.increment();
+console.log(`Результат инкремента ${myAccumulator.value}`);
+myAccumulator.increment();
+console.log(`Результат инкремента ${myAccumulator.value}`);
+myAccumulator.increment();
+console.log(`Результат инкремента ${myAccumulator.value}`);
+myAccumulator.decrement();
+console.log(`Результат декримента ${myAccumulator.value}`);
+myAccumulator.decrement();
+console.log(`Результат декримента ${myAccumulator.value}`);
+
+console.log("Екземпляр прототипа CancelableAccumulator");
+const mySecondAccumulator = new CancelableAccumulator(25);
+console.log(`Переданное значение ${mySecondAccumulator.value}`);
+mySecondAccumulator.increment();
+console.log(`Результат инкремента ${mySecondAccumulator.value}`);
+mySecondAccumulator.increment();
+console.log(`Результат инкремента ${mySecondAccumulator.value}`);
+mySecondAccumulator.increment();
+console.log(`Результат инкремента ${mySecondAccumulator.value}`);
+mySecondAccumulator.decrement();
+console.log(`Результат декримента ${mySecondAccumulator.value}`);
+mySecondAccumulator.decrement();
+console.log(`Результат декримента ${mySecondAccumulator.value}`);
+console.log(
+  `Возврат переданного в конструтор значения, переданное значение равно: ${mySecondAccumulator.clear()}`
+);
