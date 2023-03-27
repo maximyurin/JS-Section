af706c623d5288c03c92aebd661e0b86c23f0e26
Maxim <maximyurinmusic@gmail.com>
2023-03-27 23:04:59 +0300

    Homework 7 finished on 95%

--- main.js
+++ main.js
@@ -20,28 +20,78 @@ let testArray = [
   22,
 ];
 const container = document.querySelector(".results");
+let firstNumberColor;
+if (firstNumber < -500) {
+  firstNumberColor = "#650505";
+} else if (firstNumber < -100) {
+  firstNumberColor = "#b20e0e";
+} else if (firstNumber < -50) {
+  firstNumberColor = "#ee2e2e";
+} else if (firstNumber < 0) {
+  firstNumberColor = "#ff7c7c";
+} else if (firstNumber <= 50) {
+  firstNumberColor = "#0cb20c";
+} else if (firstNumber <= 100) {
+  firstNumberColor = "#087c08";
+} else if (firstNumber <= 500) {
+  firstNumberColor = "#0f520f";
+} else {
+  firstNumberColor = "#053605";
+}
+let secondNumberColor;
+if (secondNumber < -500) {
+  secondNumberColor = "#650505";
+} else if (secondNumber < -100) {
+  secondNumberColor = "#b20e0e";
+} else if (secondNumber < -50) {
+  secondNumberColor = "#ee2e2e";
+} else if (secondNumber < 0) {
+  secondNumberColor = "#ff7c7c";
+} else if (secondNumber <= 50) {
+  secondNumberColor = "#0cb20c";
+} else if (secondNumber <= 100) {
+  secondNumberColor = "#087c08";
+} else if (secondNumber <= 500) {
+  secondNumberColor = "#0f520f";
+} else {
+  secondNumberColor = "#053605";
+}
 container.insertAdjacentHTML(
   "beforeend",
-  `<div class="first-number"><h2>Первое число: <span style="color: ${
-    firstNumber < 0 ? "red" : "green"
-  }">${firstNumber}</span></h2></div>`
+  `<div class="first-number"><h2>Первое число: <span style="color: ${firstNumberColor}">${firstNumber}</span></h2></div>`
 );
 container.insertAdjacentHTML(
   "beforeend",
-  `<div class="second-number"><h2>Второе число: <span style="color: ${
-    secondNumber < 0 ? "red" : "green"
-  }">${secondNumber}</span></h2></div>`
+  `<div class="second-number"><h2>Второе число: <span style="color: ${secondNumberColor}">${secondNumber}</span></h2></div>`
 );
 
 function sum(num1, num2) {
-  if (typeof num1 === "number" && typeof num2 === "number") {
-    let result = num1 + num2;
-    let resultColor = result < 0 ? "red" : "green";
-    return container.insertAdjacentHTML(
-      "beforeend",
-      `<div class="result-of-sum"><h2>Сумма чисел: <span style="color: ${resultColor}">${result}</span></h2></div>`
-    );
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return;
   }
+  const result = num1 + num2;
+  let resultColor;
+  if (result < -500) {
+    resultColor = "#650505";
+  } else if (result < -100) {
+    resultColor = "#b20e0e";
+  } else if (result < -50) {
+    resultColor = "#ee2e2e";
+  } else if (result < 0) {
+    resultColor = "#ff7c7c";
+  } else if (result <= 50) {
+    resultColor = "#0cb20c";
+  } else if (result <= 100) {
+    resultColor = "#087c08";
+  } else if (result <= 500) {
+    resultColor = "#0f520f";
+  } else {
+    resultColor = "#053605";
+  }
+  container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="result-of-sum"><h2>Сумма чисел: <span style="color: ${resultColor}">${result}</span></h2></div>`
+  );
 }
 
 sum(firstNumber, secondNumber);
@@ -49,7 +99,24 @@ sum(firstNumber, secondNumber);
 function diff(num1, num2) {
   if (typeof num1 === "number" && typeof num2 === "number") {
     let result = num1 - num2;
-    let resultColor = result < 0 ? "red" : "green";
+    let resultColor;
+    if (result < -500) {
+      resultColor = "#650505";
+    } else if (result < -100) {
+      resultColor = "#b20e0e";
+    } else if (result < -50) {
+      resultColor = "#ee2e2e";
+    } else if (result < 0) {
+      resultColor = "#ff7c7c";
+    } else if (result <= 50) {
+      resultColor = "#0cb20c";
+    } else if (result <= 100) {
+      resultColor = "#087c08";
+    } else if (result <= 500) {
+      resultColor = "#0f520f";
+    } else {
+      resultColor = "#053605";
+    }
     return container.insertAdjacentHTML(
       "beforeend",
       `<div class="result-of-diff"><h2>Разница чисел: <span style="color: ${resultColor}">${result}</span></h2></div>`
@@ -62,9 +129,27 @@ diff(firstNumber, secondNumber);
 function multi(num1, num2) {
   if (typeof num1 === "number" && typeof num2 === "number") {
     let result = num1 * num2;
+    let resultColor;
+    if (result < -500) {
+      resultColor = "#650505";
+    } else if (result < -100) {
+      resultColor = "#b20e0e";
+    } else if (result < -50) {
+      resultColor = "#ee2e2e";
+    } else if (result < 0) {
+      resultColor = "#ff7c7c";
+    } else if (result <= 50) {
+      resultColor = "#0cb20c";
+    } else if (result <= 100) {
+      resultColor = "#087c08";
+    } else if (result <= 500) {
+      resultColor = "#0f520f";
+    } else {
+      resultColor = "#053605";
+    }
     return container.insertAdjacentHTML(
       "beforeend",
-      `<div class="result-of-multi"><h2>Произведение чисел: ${result}</h2></div>`
+      `<div class="result-of-multi"><h2>Произведение чисел: <span style="color: ${resultColor}">${result}</span></h2></div>`
     );
   }
 }
@@ -93,7 +178,9 @@ division(firstNumber, secondNumber);
 
 container.insertAdjacentHTML(
   "beforeend",
-  `<div class="array"><h2>Вводный массив: </h2><h3>[${testArray}]</h3></div>`
+  `<div class="array"><h2>Вводный массив: </h2><h3>[${testArray.join(
+    ", "
+  )}]</h3></div>`
 );
 
 let maxNumberInTestArray = -Infinity;
@@ -107,11 +194,27 @@ function maxNumber(array) {
       }
     }
   }
+  let resultColor;
+  if (maxNumberInTestArray < -500) {
+    resultColor = "#650505";
+  } else if (maxNumberInTestArray < -100) {
+    resultColor = "#b20e0e";
+  } else if (maxNumberInTestArray < -50) {
+    resultColor = "#ee2e2e";
+  } else if (maxNumberInTestArray < 0) {
+    resultColor = "#ff7c7c";
+  } else if (maxNumberInTestArray <= 50) {
+    resultColor = "#0cb20c";
+  } else if (maxNumberInTestArray <= 100) {
+    resultColor = "#087c08";
+  } else if (maxNumberInTestArray <= 500) {
+    resultColor = "#0f520f";
+  } else {
+    resultColor = "#053605";
+  }
   return container.insertAdjacentHTML(
     "beforeend",
-    `<div class="result-of-max-number"><h2>Максимальное число в массиве: <span style="color: ${
-      maxNumberInTestArray < 0 ? "red" : "green"
-    };">${maxNumberInTestArray}</span></h2></div>`
+    `<div class="result-of-max-number"><h2>Максимальное число в массиве: <span style="color: ${resultColor};">${maxNumberInTestArray}</span></h2></div>`
   );
 }
 
@@ -125,11 +228,27 @@ function minNumber(array) {
       }
     }
   }
+  let resultColor;
+  if (minNumberInTestArray < -500) {
+    resultColor = "#650505";
+  } else if (minNumberInTestArray < -100) {
+    resultColor = "#b20e0e";
+  } else if (minNumberInTestArray < -50) {
+    resultColor = "#ee2e2e";
+  } else if (minNumberInTestArray < 0) {
+    resultColor = "#ff7c7c";
+  } else if (minNumberInTestArray <= 50) {
+    resultColor = "#0cb20c";
+  } else if (minNumberInTestArray <= 100) {
+    resultColor = "#087c08";
+  } else if (minNumberInTestArray <= 500) {
+    resultColor = "#0f520f";
+  } else {
+    resultColor = "#053605";
+  }
   return container.insertAdjacentHTML(
     "beforeend",
-    `<div class="result-of-min-number"><h2>Минимальное число в массиве: <span style="color: ${
-      minNumberInTestArray < 0 ? "red" : "green"
-    };">${minNumberInTestArray}</span></h2></div>`
+    `<div class="result-of-min-number"><h2>Минимальное число в массиве: <span style="color: ${resultColor};">${minNumberInTestArray}</span></h2></div>`
   );
 }
 
