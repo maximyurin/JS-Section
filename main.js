21d2fe8180a53293f43f79ac0232497f7d0bcb85
Maxim <maximyurinmusic@gmail.com>
2023-03-31 21:21:25 +0300

    Homework 9 is finished

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-/* JavaScripts homeworks 1 */
+/* JavaScripts homeworks 8 */
 
-let firstNumber = prompt("Введите первое число");
-let secondNumber = prompt("Введите второе число");
+// 1
 
-if (firstNumber && secondNumber) {
-  let num1 = Number(firstNumber);
-  let num2 = Number(secondNumber);
-  alert(`Сумма ваших чисел: ${num1 + num2}`);
-  alert(`Разница ваших чисел: ${num1 - num2}`);
-  alert(`Результат умножения ваших чисел: ${num1 * num2}`);
-  alert(`Результат деления ваших чисел: ${num1 / num2}`);
-  alert(`Максимальное число из введённых вами чисел: ${Math.max(num1, num2)}`);
-  alert(`Первое число: ${num1 % 2 === 0 ? "чётное" : "не чётное"}`);
-  alert(`Второе число: ${num2 % 2 === 0 ? "чётное" : "не чётное"}`);
-} else {
-  alert("Одно из значений не было введено");
+function calculateAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+
+let avg = calculateAverage(numbers);
+console.log("The average is: " + avg);
+
+// 2
+
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+User.prototype.sayHello = function () {
+  return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
+};
+
+const user = new User("Sherlock", 27);
+console.log(user.sayHello());
+
+// 3
+
+const c = calculator();
+
+console.log(c());
+console.log(c());
+
+function calculator(init = 0) {
+  init++;
+  return function () {
+    return init++;
+  };
 }
