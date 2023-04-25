9000908c42c2f9249737293493414ee17e25f9ab
Maxim <maximyurinmusic@gmail.com>
2023-04-25 12:54:05 +0300

    Added some changes

--- main.js
+++ main.js
@@ -4,28 +4,55 @@
 // 3. Написати метод getRepos(), який буде робити запит на сервер та повертати результат. В запиті потрібно додати заголовок з вашим токеном (https://docs.github.com/en/rest/overview/authenticating-to-the-rest-api?apiVersion=2022-11-28#basic-authentication). Документація, як використовувати header в fetch (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch).
 // 4. Створити новий екземпляр класу та використати метод з п3 при завантаженні сторінки.
 
-const token = "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";
-
 class GithubApi {
   constructor(token, username) {
     this.token = token;
-    this.userName = username;
+    this.username = username;
   }
   async getRepos() {
-    const response = await fetch(
-      `https://api.github.com/users/${this.userName}/repos`,
-      {
-        headers: {
-          Authorization: `token ${this.token}`,
-        },
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${this.username}/repos`,
+        {
+          headers: {
+            Authorization: `token ${this.token}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        if (response.status === 401) {
+          // Повторный запрос на обновление токена
+          const newToken = await this.updateToken();
+          this.token = newToken;
+          return this.getRepos();
+        }
+        throw new Error("Ошибка при выполнении запроса");
       }
-    );
-    return await response.json();
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async updateToken() {
+    // Запрос на обновление токена
+    const response = await fetch(`https://api.github.com/token/update`, {
+      headers: {
+        Authorization: `token ${this.token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Ошибка при обновлении токена");
+    }
+    const data = await response.json();
+    return data.token;
   }
 }
 
 window.addEventListener("load", async () => {
+  const token =
+    "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";
   const githubApi = new GithubApi(token, "maximyurin");
-  const repositories = await githubApi.getRepos();
-  console.log(repositories);
+  const repos = await githubApi.getRepos();
+  console.log(repos);
 });
